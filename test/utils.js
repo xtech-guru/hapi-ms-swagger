8423ba5c85b0dd6a062deba90f8d3f248e8b5aa9
Haykel Ben Jemia <hbj@allmas-tn.com>
2016-07-10 16:25:35 +0100

    In test utils, allow 'send' option to return a promise and add the result text as a note on error.

--- test/utils.js
+++ test/utils.js
@@ -1,5 +1,6 @@
 'use strict';
 
+const Promise = require('bluebird');
 const chai = require('chai');
 const expect = chai.expect;
 
@@ -14,20 +15,24 @@ exports.request = function(url, verb, config) {
   const req = chai.request(app)[verb](url);
   const send = (typeof config.send === 'function') ? config.send() : config.send;
 
-  if (send)
-    req.send(send);
+  return Promise
+    .resolve(send)
+    .then(send => {
+      if (send)
+        req.send(send);
 
-  return req;
+      return req;
+    });
 };
 
 exports.createTests = function(lab, url, verb, configs) {
   configs.forEach((config) => {
     const title = config.title || ('should respond with ' + config.status);
 
-    lab.it(title, config.options || {}, function() {
+    lab.it(title, config.options || {}, function(done) {
       const finalUrl = (typeof url === 'function') ? url() : url;
 
-      return exports
+      exports
         .request(finalUrl, verb, config)
         .catch((err) => {
           // catch http response errors and pass the responses for checking
@@ -37,17 +42,25 @@ exports.createTests = function(lab, url, verb, configs) {
           throw err;
         })
         .then((res) => {
-          expect(res).to.have.status(config.status);
+          return Promise
+            .try(function() {
+              expect(res).to.have.status(config.status);
 
-          if (config.contentType)
-            expect(res).to.have.header('content-type', exports.contentTypes[config.contentType] || config.contentType);
+              if (config.contentType)
+                expect(res).to.have.header('content-type', exports.contentTypes[config.contentType] || config.contentType);
 
-          if (typeof config.check === 'function')
-            config.check(res);
+              if (typeof config.check === 'function')
+                config.check(res);
 
-          return null;
-        });
+              return null;
+            })
+            .catch(err => {
+              done.note(`res.text: ${res.text}`);
 
+              throw err;
+            })
+        })
+        .asCallback(done);
     });
   });
 };
